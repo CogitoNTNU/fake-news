1079683b1894d8f51f950676633aafd6b8bcb23f
Espen Fosseide <espenbfo@stud.ntnu.no>
2020-02-26 13:24:57 +0100

    fungerende applikasjon!

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ import model as md
 data = ""
 atestset = []
 labels = []
-output_size = 3500
+VOCABULARY_SIZE = 3500
 WORD_COUNT = 50
-LOAD_WEIGHT=True
-WEIGHT_FILE = "weights0.h5"
+LOAD_WEIGHT=False
+WEIGHT_FILE = "weights1.h5"
 categoric = ""
 
 dataset = pd.read_json("./dataSet.json").to_numpy()
 dataset = list(dataset)
-v_size = 3500
 new_dataset = []
 labels = []
 for i in range(len(dataset)):
@@ -26,19 +25,17 @@ for i in range(len(dataset)):
         new_dataset +=[[0]*(WORD_COUNT-len(dataset[i][:]))+list(dataset[i][:-1])]
         labels+=[dataset[i][-1]]
         dataset[i] = dataset[i][:-1]
-print(labels)
 new_dataset = np.array(new_dataset)
 print(new_dataset.shape)
 #labels=[dataset[i][49] for i in range(len(dataset))]
 #dataset = np.array([dataset[i][0:49] for i in range(len(dataset))])
 
-labels = to_categorical(labels,v_size)
-print(labels)
-model = md.generateModel()
+labels = to_categorical(labels,VOCABULARY_SIZE)
+model = md.generateModel5()
 if (LOAD_WEIGHT):
     model.load_weights(WEIGHT_FILE)
 print(model.summary())
 # fit the model
 for i in range(10):
-    model.fit(new_dataset, labels, validation_split=0.1, epochs=2)
+    model.fit(new_dataset, labels, validation_split=0.1, epochs=2,batch_size=128)
     model.save_weights("weights" + str(i) + ".h5")
--- model.py
+++ model.py
@@ -12,14 +12,15 @@ dataDic = []
 with open("./dataDic.json") as f:
     dataDic = json.load(f)
 
+EMBEDDING_FILE="C:/Program Files (x86)/Cogito Data/glove/glove.twitter.27B.100d.txt"
 def loadEmbeddings():
     embeddings_index = np.zeros(shape=(VOCABULARY_SIZE, 100))
 
-    f = open('C:/Program Files (x86)/Cogito Data/glove/glove.6B.100d.txt', encoding="UTF-8")
+    f = open(EMBEDDING_FILE, encoding="UTF-8")
     for line in f:
         values = line.split()
         word = values[0]
-        coefs = np.asarray(values[1:], dtype='float32')
+        coefs = np.asarray(values[1:], dtype='float16')
         try:
             embeddings_index[dataDic.index(word)] = coefs
         except Exception as e:
@@ -48,12 +49,64 @@ def generateModel2():
     e = Embedding(VOCABULARY_SIZE, 100, weights=[embeddings_index], input_length=49, trainable=False)
     model.add(e)
     #model.add(Masking(mask_value=0.0))
-    model.add(CuDNNLSTM(100,return_sequences=True))
+    model.add(CuDNNLSTM(400,return_sequences=True))
     model.add(CuDNNLSTM(300))
-    model.add(Dense(300,activation='relu'))
+    model.add(Dense(200,activation='relu'))
     #model.add(BatchNormalization())
-    model.add(Dense(300, activation='relu'))
+    model.add(Dense(100, activation='relu'))
     model.add(Dropout(0.2))
+    model.add(Dense(VOCABULARY_SIZE, activation='sigmoid'))
+    # compile the model
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def generateModel3():
+    embeddings_index = loadEmbeddings()
+
+    model = Sequential()
+    e = Embedding(VOCABULARY_SIZE, 100, weights=[embeddings_index], input_length=49, trainable=False)
+    model.add(e)
+    #model.add(Masking(mask_value=0.0))
+    model.add(CuDNNLSTM(50,return_sequences=True))
+    model.add(CuDNNLSTM(100))
+    model.add(Dense(100,activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Dense(VOCABULARY_SIZE, activation='sigmoid'))
+    # compile the model
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def generateModel4():
+    embeddings_index = loadEmbeddings()
+
+    model = Sequential()
+    e = Embedding(VOCABULARY_SIZE, 100, weights=[embeddings_index], input_length=49, trainable=False)
+    model.add(e)
+    #model.add(Masking(mask_value=0.0))
+    model.add(CuDNNLSTM(128))
+    model.add(Dense(200,activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Dense(VOCABULARY_SIZE, activation='sigmoid'))
+    # compile the model
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def generateModel5():
+    embeddings_index = loadEmbeddings()
+
+    model = Sequential()
+    e = Embedding(VOCABULARY_SIZE, 100, weights=[embeddings_index], input_length=49, trainable=False)
+    model.add(e)
+    #model.add(Masking(mask_value=0.0))
+    model.add(CuDNNLSTM(128))
+    model.add(Dense(200,activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
     model.add(Dense(VOCABULARY_SIZE, activation='sigmoid'))
     # compile the model
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
